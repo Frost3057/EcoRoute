eaf0d1154567612d9775c09be9de1aa91ba9a496
Aman Bajpai <amanbajpai5660@gmail.com>
2025-07-04 01:32:01 +0530

    local block chain network is live

--- BlockChain/init.py
+++ BlockChain/init.py
@@ -2,15 +2,19 @@
 import Api
 from Api.api import install_Client,load_env
 from Api.multichain import MultiChainClient
+import blockChain
+from blockChain import blockChain
+import json
 def init():
     install_Client()
     rpchost='127.0.0.1'
     rpcport=1234
     creds = load_env()
-    print(creds)
     mc=MultiChainClient(rpchost, rpcport, creds[0], creds[1])
     result = mc.getinfo()
-    print(f"Block chain details : {result}")     
+    print(f"Block chain details -> Chain Name = {result["chainname"]} || Description = {result["description"]}") 
+    client = blockChain(model = mc)
+    client.getItems('Inventory')
 def main():
     init()
 if __name__ == "__main__":
