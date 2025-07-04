49b5e98d5710e1bb625a5c113f9f4f4b54b3dc28
Aman Bajpai <amanbajpai5660@gmail.com>
2025-07-04 19:39:00 +0530

    EcoCoin transactions enabled

--- BlockChain/init.py
+++ BlockChain/init.py
@@ -14,7 +14,15 @@ def init():
     result = mc.getinfo()
     print(f"Block chain details -> Chain Name = {result["chainname"]} || Description = {result["description"]}") 
     client = blockChain(model = mc)
-    client.getItems('Inventory')
+    senderadd = '1CyKBraiJ7yw7QQfjvDrVdH1ai9oroWCrDfxwX'
+    receiveradd = '1Aej3ZBacU1JGtTTzAz4NfqvoanHrrN6Gaq3Ce'
+    # client.sendPermi(add)
+    client.receivePermi(receiveradd)
+    print(f"Sender Balance: {client.getbalance(senderadd)}")
+    print(f"Receiver Balance:{client.getbalance(receiveradd)}")
+    client.transaction(senderadd,receiveradd,0.1)
+    print(f"Sender Balance: {client.getbalance(senderadd)}")
+    print(f"Receiver Balance:{client.getbalance(receiveradd)}")
 def main():
     init()
 if __name__ == "__main__":
